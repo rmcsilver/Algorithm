77d95613477f453da33adcb4ca18efc4ac39df04
silver <taijiofx@gmail.com>
2020-04-28 05:47:48 +0000

    Queue에서 new로 동적 할당, overflow 방지 코드 추가 ,코드 컨벤션 수정

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -1,66 +1,99 @@
 #include <iostream>
 
-using namespace std;
-
-const int MAX_LENGTH = 256;
-
-class Queue{
+class Queue
+{
 private:
-    int DataList[MAX_LENGTH];
+    int * DataList;
     int FrontIndex;
     int BackIndex;
+    int Length;
 
 public:
-    Queue();
+    Queue(int InLength = 0);
+    ~Queue();
     bool IsEmpty();
-    void PushData(int InData);
+    bool PushData(int InData);
     int PopData();
     int GetSize();
+    void PrintData();
 };
 
-Queue::Queue(){
+Queue::Queue(int InLength)
+{
+    DataList = new int[InLength];
+    Length = InLength;
     FrontIndex = 0;
     BackIndex = 0;
 }
 
-bool Queue::IsEmpty(){
+Queue::~Queue()
+{
+    delete [] DataList;
+    DataList = nullptr;
+}
+
+bool Queue::IsEmpty()
+{
     return FrontIndex == BackIndex;
 }
 
-void Queue::PushData(int InData){
+bool Queue::PushData(int InData)
+{
+    if(this->GetSize() >= Length)
+    {
+        std::cout << "큐가 가득찼습니다." << std::endl;
+        return false;
+    }
+
     DataList[BackIndex] = InData;
     BackIndex += 1;
+    return true;
 }
 
-int Queue::PopData(){
+int Queue::PopData()
+{
+    if(this->GetSize() == 0)
+    {
+        std::cout << "큐에 데이터가 없습니다." << std::endl;
+    }
+
     int OutData = DataList[FrontIndex];
     FrontIndex += 1;
     return OutData;
 }
 
-int Queue::GetSize(){
+int Queue::GetSize()
+{
     return BackIndex - FrontIndex;
+}
+
+void Queue::PrintData()
+{
+    for(int i=0; i< this->GetSize(); ++i)
+    {
+        std::cout << i << "번째 데이터 : " << DataList[i] << std::endl;
+    }
 }
 
 int main(){
     
-    Queue QueueList;
+    Queue QueueList(10);
 
-    std::cout << "큐의 현재 사이즈 :" << QueueList.GetSize() << endl; 
+    std::cout << "큐의 현재 사이즈 :" << QueueList.GetSize() << std::endl; 
 
-    for(int i=0; i<10; i++)
+    for(int i=0; i<11; ++i)
     {
         QueueList.PushData(i);
-        std::cout << "push() : " << i << endl;
     }
 
-    std::cout << "큐의 현재 사이즈 :" << QueueList.GetSize() << endl; 
+    QueueList.PrintData();
+    std::cout << "큐의 현재 사이즈 :" << QueueList.GetSize() << std::endl; 
 
-    for(int j=0; j<5; j++)
+    for(int j=0; j<5; ++j)
     {
-        std::cout << "pop() : " << QueueList.PopData() << endl;
+        QueueList.PopData();
     }
 
-    std::cout << "큐의 현재 사이즈 :" << QueueList.GetSize() << endl; 
-
+    QueueList.PrintData();
+    std::cout << "큐의 현재 사이즈 :" << QueueList.GetSize() << std::endl; 
 }
